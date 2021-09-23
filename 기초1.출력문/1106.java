8ddb2e48d42e1b7eb995835096a66dfa71ac9f90
Mason Na <bohemianmoon5@gmail.com>
2021-09-23 13:04:00 +0900

    기초1.출력문

---
+++ 기초1.출력문/1106.java
@@ -0,0 +1,12 @@
+﻿public class Main {
+    
+    public static void main(String[] args) {
+        
+        int a = -(int)(Math.pow(2,32)) - 1;
+        int b = (int)(Math.pow(2,32));
+        
+        System.out.printf("%d %d", a, b);
+        
+    }
+    
+}
