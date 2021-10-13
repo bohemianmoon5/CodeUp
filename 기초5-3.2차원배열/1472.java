3f1c4129c666049b6c54a6723a62d2c288aed663
Mason Na <bohemianmoon5@gmail.com>
2021-10-13 18:17:19 +0900

    기초5-3.2차원배열

---
+++ 기초5-3.2차원배열/1472.java
@@ -0,0 +1,36 @@
+﻿import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int n = sc.nextInt();
+	    int m = sc.nextInt();
+	    
+	    int[][] a = new int[n][m];
+	    int k = 1;
+	    
+	    for(int i = 0; i < n; i++) {
+	        for(int j = 0; j < m; j++) {
+	            a[i][j] = k++;
+	        }
+	    }
+	    
+	    for(int i = n-1; i > -1; i--) {
+	        if(i%2 != 0) {
+	            for(int j = 0; j < m; j++) {
+	                System.out.printf("%d ", a[i][j]);
+	            }
+	        }else {
+	            for(int j = m-1; j > -1; j--) {
+	                System.out.printf("%d ", a[i][j]);
+	            }
+	        }
+	        System.out.println();
+	    }
+
+	}
+
+}
---
+++ 기초5-3.2차원배열/1475.java
@@ -0,0 +1,38 @@
+﻿import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int n = sc.nextInt();
+	    int m = sc.nextInt();
+	    
+	    int[][] a = new int[110][110];
+	    int k = 1;
+	    int t = 1;
+	    
+	    for(int i = m-1; i > -1; i--) {
+	        if(t%2 != 0) {
+	            for(int j = 0; j < n; j++) {
+	                a[j][i] = k++;
+	            }
+	        }else {
+	            for(int j = n-1; j > -1; j--) {
+	                a[j][i] = k++;
+	            }
+	        }
+	        t++;
+	    }
+	    
+	    for(int i = 0; i < n; i++) {
+	        for(int j = 0; j < m; j++) {
+	            System.out.printf("%d ", a[i][j]);
+	        }
+	        System.out.println();
+	    }
+
+	}
+
+}
