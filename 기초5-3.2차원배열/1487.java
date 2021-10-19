316009695cc19ee03ce99750702b3f05230b6fa7
Mason Na <bohemianmoon5@gmail.com>
2021-10-19 23:50:27 +0900

    기초5-3.2차원배열

---
+++ 기초5-3.2차원배열/1487.java
@@ -0,0 +1,44 @@
+﻿import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int n = sc.nextInt();
+	    int m = sc.nextInt();
+	    
+	    int[][] a = new int[110][110];
+	    
+	    int x = -1, y = m-1, t = n*m+1, k = 1, e = n, f = m; 
+	    
+	    while(true) {
+	        for(int i = 0; i < n; i++) {
+	            t--;
+	            x += k;
+	            a[x][y] = t;
+	        }
+	        n--;
+	        m--;
+	        k *= -1;
+	        for(int j = 0; j < m; j++) {
+	            t--;
+	            y += k;
+	            a[x][y] = t;
+	        }
+	        if(n <= 0 || m <= 0) {
+	            break;
+	        }
+	    }
+	    
+	    for(int i = 0; i < e; i++) {
+	        for(int j = 0; j < f; j++) {
+	            System.out.printf("%d ", a[i][j]);
+	        }
+	        System.out.println();
+	    }
+
+	}
+
+}
---
+++ 기초5-3.2차원배열/1488.java
@@ -0,0 +1,44 @@
+﻿import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int n = sc.nextInt();
+	    int m = sc.nextInt();
+	    
+	    int[][] a = new int[110][110];
+	    
+	    int x = n-1, y = m, t = 0, k = 1, e = n, f = m; 
+	    
+	    while(true) {
+	        k *= -1;
+	        for(int i = 0; i < m; i++) {
+	            t++;
+	            y += k;
+	            a[x][y] = t;
+	        }
+	        n--;
+	        m--;
+	        for(int j = 0; j < n; j++) {
+	            t++;
+	            x += k;
+	            a[x][y] = t;
+	        }
+	        if(n <= 0 || m <= 0) {
+	            break;
+	        }
+	    }
+	    
+	    for(int i = 0; i < e; i++) {
+	        for(int j = 0; j < f; j++) {
+	            System.out.printf("%d ", a[i][j]);
+	        }
+	        System.out.println();
+	    }
+
+	}
+
+}
