0b8e3aa1db86d2da9623aec1623898d7743fe850
Mason Na <bohemianmoon5@gmail.com>
2021-10-20 23:35:53 +0900

    기초5-3.2차원배열

---
+++ 기초5-3.2차원배열/1492.java
@@ -0,0 +1,24 @@
+﻿import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int n = sc.nextInt();
+	    int[] a = new int[n];
+	    int total = 0;
+	    
+	    for(int i = 0; i < n; i++) {
+	        a[i] = sc.nextInt();
+	    }
+	    
+	    for(int i = 0; i < n; i++) {
+	        total += a[i];
+	        System.out.printf("%d ", total);
+	    }
+
+	}
+
+}
