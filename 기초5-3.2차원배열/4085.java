3ea36bff6701f6590cc48a377adcec24bc51436c
Mason Na <bohemianmoon5@gmail.com>
2021-11-04 11:48:25 +0900

    기초5-3.2차원배열

---
+++ 기초5-3.2차원배열/4085.java
@@ -0,0 +1,42 @@
+﻿import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int[][] map = new int[101][101];
+	    
+	    int m = sc.nextInt();
+	    int n = sc.nextInt();
+	    int x = sc.nextInt();
+	    int y = sc.nextInt();
+	    
+	    for(int i = 1; i <= n; i++) {
+	        for(int j = 1; j <= m; j++) {
+	            map[i][j] = sc.nextInt();
+	        }
+	    }
+	    
+	    int sum = 0;
+	    int t = 0;
+	    
+	    for(int i = 1; i <= n-y+1; i++) {
+	        for(int j = 1; j <= m-x+1; j++) {
+	            for(int k = 0; k < y; k++) {
+	                for(int d = 0; d < x; d++) {
+	                    sum += map[i+k][j+d];
+	                }
+	            }
+	            if(sum > t) {
+	                t = sum;
+	            }
+	            sum = 0;
+	        }
+	    }
+	    System.out.println(t);
+
+	}
+
+}
