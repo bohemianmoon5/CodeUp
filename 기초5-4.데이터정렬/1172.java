2a09e9815171d6c79675863e64b3ed35e823a66a
Mason Na <bohemianmoon5@gmail.com>
2021-11-04 11:53:46 +0900

    기초5-4.데이터정렬

---
+++ 기초5-4.데이터정렬/1172.java
@@ -0,0 +1,30 @@
+﻿import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+     
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        int[] arr = new int[3];
+        arr[0] = a;
+        arr[1] = b;
+        arr[2] = c;
+        
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = i + 1; j < arr.length; j++) {
+                if(!(arr[i] < arr[j])) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }   
+        }
+        System.out.printf("%d %d %d", arr[0], arr[1], arr[2]);
+    }    
+    
+}
