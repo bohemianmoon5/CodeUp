bcdcc92fd2f017a2469d09dd8a912b8905325a01
Mason Na <bohemianmoon5@gmail.com>
2021-11-06 22:42:05 +0900

    기초5-4.데이터정렬

---
+++ 기초5-4.데이터정렬/1441.java
@@ -0,0 +1,35 @@
+﻿import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+        
+        //버블정렬
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int[] a = new int[10001];
+        
+        int n = sc.nextInt();
+        
+        for(int i = 1; i <= n; i++) {
+            a[i] = sc.nextInt();
+        }
+        
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j <= n-1; j++) {
+                if(a[j] > a[j+1]) {
+                    int t = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = t;
+                }
+            }
+        }
+        
+        for(int i = 1; i <= n; i++) {
+            System.out.println(a[i]);
+        }
+        
+	}
+
+}
---
+++ 기초5-4.데이터정렬/1442.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	    //선택정렬
+	    
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int[] a = new int[10001];
+	    
+	    int n = sc.nextInt();
+	    
+	    for(int i = 1; i <= n; i++) {
+	        a[i] = sc.nextInt();
+	    }
+	    
+	    for(int i = 1; i < n; i++) {
+	        int m = i;
+	        for(int j = i+1; j <= n; j++) {
+	            if(a[j] < a[m]) {
+	                m = j;
+	            }
+	        }
+	        int t = a[i];
+	        a[i] = a[m];
+	        a[m] = t;
+	    }
+	    
+	    for(int i = 1; i <= n; i++) {
+	        System.out.println(a[i]);
+	    }
+
+	}
+
+}
