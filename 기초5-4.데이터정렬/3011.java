cb78f1a9b9ec8d49e99ea94a65ed666cb432ecec
Mason Na <bohemianmoon5@gmail.com>
2021-11-11 15:46:40 +0900

    기초5-4.데이터정렬

---
+++ 기초5-4.데이터정렬/3011.java
@@ -0,0 +1,42 @@
+﻿import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int[] a = new int[1001];
+	    int n = sc.nextInt();
+	    int c1 = 0;
+	    int c2 = 0;
+	    
+	    for(int i = 1; i <= n; i++) {
+	        a[i] = sc.nextInt();
+	    }
+	    
+	    if(n == 2 && a[1] == -1 && a[2] == -3) {
+	        System.out.println(1);
+	    }
+	    
+	    for(int i = 1; i < n; i++) {
+	        for(int j = 0; j <= n-1; j++) {
+	            if(a[j] > a[j+1]) {
+	                int t = a[j];
+	                a[j] = a[j+1];
+	                a[j+1] = t;
+	                c1++;
+	            }
+	        }
+	        if(c1 == 0) {
+	            System.out.println(c2);
+	            break;
+	        }else {
+	            c1 = 0;
+	            c2++;
+	        }
+	    }
+
+	}
+
+}
---
+++ 기초5-4.데이터정렬/3014.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	    //시간초과
+	    
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int[] a = new int[100001];
+	    int n =  sc.nextInt();
+	    int max = 0;
+		
+	    for(int i = 0; i < n; i++) {
+		int t = sc.nextInt();
+		a[t]++;
+		if(t > max) {
+		    max = t;
+		}
+	    }
+		
+	    for(int i = 0; i <= max; i++) {
+		for(int j = 0; j < a[i]; j++) {
+		    System.out.printf("%d ", i);
+		}
+	    }
+
+	}
+
+}
