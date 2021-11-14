350220725eb3266f74e940792dadcdc8e8cc81fc
Mason Na <bohemianmoon5@gmail.com>
2021-11-14 19:59:18 +0900

    기초5-4.데이터정렬

---
+++ 기초5-4.데이터정렬/4501.java
@@ -0,0 +1,22 @@
+﻿import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner(System.in);
+	    
+	    List<Integer> list = new ArrayList<>();
+	    
+	    for(int i = 0; i < 7; i++) {
+	        list.add(sc.nextInt());
+	    }
+	    
+	    Collections.sort(list);
+	    Collections.reverse(list);
+	    
+	    System.out.printf("%d\n%d", list.get(0), list.get(1));
+	    
+	}
+
+}
---
+++ 기초5-4.데이터정렬/4701.java
@@ -0,0 +1,46 @@
+﻿import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int n = sc.nextInt();
+	    int[] a = new int[n];
+	    
+	    for(int i = 0; i < n; i++) {
+	        a[i] = sc.nextInt();
+	    }
+	    
+	    Arrays.sort(a);
+	    
+	    int i = 0;
+	    int j = a.length - 1;
+	    
+	    int gap = Integer.MAX_VALUE;
+	    int result1 = 0;
+	    int result2 = 0;
+	    
+	    int temp;
+	    int sum;
+	    
+	    while(i < j) {
+	        sum = a[i] + a[j];
+	        temp = Math.abs(sum);
+	        if(temp < gap) {
+	            gap = temp;
+	            result1 = a[i];
+	            result2 = a[j];
+	        }
+	        if(sum > 0) {
+	            j--;
+	        }else {
+	            i++;
+	        }
+	    }
+	    System.out.printf("%d %d", result1, result2);
+
+	}
+
+}
