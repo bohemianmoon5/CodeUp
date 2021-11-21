f61bbb59915eabcb70505c18de3c5795d09f5650
Mason Na <bohemianmoon5@gmail.com>
2021-11-21 23:01:58 +0900

    기초6.함수

---
+++ 기초6.함수/1602.java
@@ -0,0 +1,27 @@
+﻿import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Main {
+    
+    static Double d;
+
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner(System.in);
+	    
+	    d = sc.nextDouble();
+	    
+	    DecimalFormat df = new DecimalFormat("#.##########");
+	    
+	    System.out.println(df.format(abs(d)));
+
+	}
+	static Double abs(Double d) {
+	    if(d >= 0) {
+	        return d;
+	    }else {
+	        return -d;
+	    }
+	}
+
+}
---
+++ 기초6.함수/1610.java
@@ -0,0 +1,23 @@
+﻿import java.util.Scanner;
+
+public class Main {
+    
+    static String s;
+    static int f, e;
+
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner(System.in);
+	    
+	    s = sc.nextLine();
+	    f = sc.nextInt();
+	    e = sc.nextInt();
+	    
+	    System.out.println(mysubstr(s, f, e));
+
+	}
+	static String mysubstr(String s, int f, int e) {
+	    return (String) s.subSequence(f, f+e);
+	}
+
+}
