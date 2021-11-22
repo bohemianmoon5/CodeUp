26d138c992d79d4abd08db37b08cc7b86677c8f5
Mason Na <bohemianmoon5@gmail.com>
2021-11-22 16:09:28 +0900

    기초6.함수

---
+++ 기초6.함수/1615.java
@@ -0,0 +1,47 @@
+﻿import java.util.Scanner;
+
+public class Main {
+    
+    static boolean[] a;
+    static int f, e;
+
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner(System.in);
+	    
+	    a = new boolean[5001];
+	    
+	    f = sc.nextInt();
+	    e = sc.nextInt();
+	    
+	    for(int i = 1; i <= 5000; i++) {
+	        int index = self(i);
+	        if(index <= e) {
+	            a[index] = true;
+	        }
+	    }
+	    
+	    int result = 0;
+	    
+	    for(int i = f; i <= e; i++) {
+	        if(!a[i]) {
+	            result += i;
+	        }
+	    }
+	    
+	    System.out.printf("%d", result);
+
+	}
+	static int self(int n) {
+	    int sum = n;
+	    while(true) {
+	        if(n == 0) {
+	            break;
+	        }
+	        sum += n % 10;
+	        n = n / 10;
+	    }
+	    return sum;
+	}
+
+}
---
+++ 기초6.함수/1620.java
@@ -0,0 +1,39 @@
+﻿import java.util.Scanner;
+
+public class Main {
+    
+    static int n;
+
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner(System.in);
+	    
+	    n = sc.nextInt();
+	    
+	    while(true) {
+	        if(n < 10) {
+	            break;
+	        }
+	        n = f(n);
+	    }
+	    
+	    System.out.printf("%d", n);
+
+	}
+	static int f(int n) {
+	    
+	    int value = n;
+	    int result = 0;
+	    
+	    while(true) {
+	        if(value == 0) {
+	            break;
+	        }
+	        result += value % 10;
+	        value /= 10;
+	    }
+	    
+	    return result;
+	}
+
+}
