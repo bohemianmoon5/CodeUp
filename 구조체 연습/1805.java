0b2ee3eb1869da42c439b35f5f96b4e8cc58aab2
Mason Na <bohemianmoon5@gmail.com>
2021-11-23 22:55:32 +0900

    구조체 연습

---
+++ 구조체 연습/1805.java
@@ -0,0 +1,22 @@
+﻿import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int n = sc.nextInt();
+	    Map<Integer, Integer> map = new TreeMap<>();
+	    
+	    for(int i = 0; i < n; i++) {
+	        map.put(sc.nextInt(), sc.nextInt());
+	    }
+	    
+	    for(Integer key : map.keySet()) {
+	        System.out.printf("%d %d\n", key, map.get(key));
+	    }
+
+	}
+
+}
